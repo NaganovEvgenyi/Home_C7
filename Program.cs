fe521f7bf10c073ebd9e6912ecbe04a9945f19f1
NaganovEvgenyi <enaganov@inbox.ru>
2023-02-02 22:50:57 +0300

    3 задача

--- Program.cs
+++ Program.cs
@@ -85,3 +85,23 @@ void PrintArray(int[,] Array)
         Console.WriteLine();
     }
 }
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] Array = GetArray(3, 3, 0, 10);
+PrintArray(Array);
+
+for (int j = 0; j < Array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        sum += Array[i, j];
+    }
+    Console.Write($"{ sum / Array.GetLength(0)}; ");
+}
